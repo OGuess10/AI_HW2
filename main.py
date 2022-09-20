41c85ea3bf29c9506e3345d6782dbd1211b9c399
Olivia <guesso.olivia@gmail.com>
2022-09-20 13:43:52 -0500

    setup actually

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 
 from array import *
 
+class Room():
+     def __init__(self, a, b):
+        self.vac = a
+        self.dirt = b 
+
 #Rooms
 ROOM_TOTAL = 20
 ROOM_ROWS = 4
@@ -19,29 +24,33 @@ SUCK = 0.6
 def main():
 
     #x = vacuum(0,1), y = dirt(0,1)
-    rooms = [[(0,0) for i in range(ROOM_COLS)] for j in range(ROOM_ROWS)]
+    rooms = [[Room(False,False) for i in range(ROOM_COLS)] for j in range(ROOM_ROWS)]
 
     #instance 1
     init_loc = (2,2)
     dirty_squares = [(1,2), (2,4), (3,5)]
     for dirt in dirty_squares:
-        rooms[dirt.x][dirt.y].y = 1
-    rooms[init_loc.x][init_loc.y].x = 1
-    uniform_cost_tree_search(rooms)
-    uniform_cost_graph_search(rooms)
-    iterative_deepening_tree_search(rooms)
+       rooms[dirt[0]-1][dirt[1]-1].dirt = True
+    rooms[init_loc[0]-1][init_loc[1]-1].vac = True
+    # for x in range(ROOM_ROWS):
+    #     for y in range(ROOM_COLS):
+    #         print(rooms[x][y].vac, end='')
+    #         print(",", end='') 
+    #         print(rooms[x][y].dirt, end=" ")
+    #     print()
+    # uniform_cost_tree_search(rooms)
+    # uniform_cost_graph_search(rooms)
+    # iterative_deepening_tree_search(rooms)
 
      #x = vacuum(0,1), y = dirt(0,1)
-    rooms = [[(0,0) for i in range(ROOM_COLS)] for j in range(ROOM_ROWS)]
+    rooms = [[Room(False,False) for i in range(ROOM_COLS)] for j in range(ROOM_ROWS)]
 
     #instance 2
-    init_loc = (3,2)
-    dirty_squares = [(1,2), (2,1), (2,4), (3,3)]
     for dirt in dirty_squares:
-        rooms[dirt.x][dirt.y].y = 1
-    rooms[init_loc.x][init_loc.y].x = 1
-    uniform_cost_tree_search(rooms)
-    uniform_cost_graph_search(rooms)
-    iterative_deepening_tree_search(rooms)
+       rooms[dirt[0]-1][dirt[1]-1].dirt = True
+    rooms[init_loc[0]-1][init_loc[1]-1].vac = True
+    # uniform_cost_tree_search(rooms)
+    # uniform_cost_graph_search(rooms)
+    # iterative_deepening_tree_search(rooms)
 
 main()
