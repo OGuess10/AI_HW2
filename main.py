b7d394d669392e555e8f3044965601ef0a802d67
Melissa Tully <91146797+Mel-Tully@users.noreply.github.com>
2022-09-24 23:04:34 -0500

    Started iterative search, added function types

--- main.py
+++ main.py
@@ -14,17 +14,14 @@ UP = 0.8
 DOWN = 0.7
 SUCK = 0.6
 
-class Tree():
-   def __init__(self, root):
-      self.root = root
-
 class Node():
-   def __init__(self, vac_loc, dirt_loc, cost):
+   def __init__(self, vac_loc: tuple, dirt_loc: list, cost: float):
       self.vac_loc = vac_loc
       self.dirt_loc = dirt_loc
       self.children = []
       self.cost = cost
       self.next_actions = ['suck']
+      self.depth = 0 # used only for Iterative Deeping Search
       if vac_loc[1] > 1:
          self.next_actions.append('left')
       if vac_loc[1] < 5:
@@ -34,7 +31,11 @@ class Node():
       if vac_loc[0] < 4:
          self.next_actions.append('down')
 
-def insert_node(parent, action):
+class Tree():
+   def __init__(self, root: Node):
+      self.root = root
+
+def insert_node(parent: Node, action: str):
    child = new_rooms_node(parent, action)
    if child != None:
       parent.children.append(child)
@@ -45,7 +46,7 @@ class min_queue:
    def __init__(self):
       self.queue = []
 
-   def insert(self, node):
+   def insert(self, node: Node):
       for i in range(len(self.queue)):
          if node.cost < self.queue[i].cost:
             break
@@ -60,7 +61,8 @@ class min_queue:
    def pop(self):
       return self.queue.pop(0)
 
-def new_rooms_node(parent, action):
+
+def new_rooms_node(parent: Node, action: str):
    child = Node((0,0),parent.dirt_loc.copy(), 0)
    if action == 'left':
       child.cost = parent.cost + 1.0
@@ -81,7 +83,7 @@ def new_rooms_node(parent, action):
       child.vac_loc = parent.vac_loc
    return child
 
-def uniform_cost_tree_search(tree):
+def uniform_cost_tree_search(tree: Tree):
    #fringe = [tree.root]
    fringe = min_queue()
    fringe.insert(tree.root)
@@ -104,15 +106,26 @@ def uniform_cost_tree_search(tree):
          fringe.insert(insert_node(next_node, action))
 
 
+def iterative_deepening_tree_search(tree: Tree):
+   #bds, but at increasing depths
+   #next node determined by lowest row, then lowest column
+   
+   max_depth = 0 # 0-indexed, root.depth = 0
+   solution = []
+   fringe = []
+
+   
+
+
 def main():
    #instance 1
    vacuum = (2,2)
    dirt = [(1,2), (2,4), (3,5)]
    tree = Tree(Node(vacuum,dirt, 0.0))
    uniform_cost_tree_search(tree)
-   # uniform_cost_graph_search(rooms)
-   # iterative_deepening_tree_search(rooms)
+   # uniform_cost_graph_search(tree)
+   # iterative_deepening_tree_search(tree)
 
-    #instance 2
+   #instance 2
 
 main()
